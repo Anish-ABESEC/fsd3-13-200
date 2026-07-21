70a7231b798164bce859ca7452c31b5fb0d5f709
Anish Singh <info.anish.in@gmail.com>
2026-07-21 14:48:59 +0530

    another commit

---
+++ Lab1/hello.js
@@ -0,0 +1,11 @@
+function sayHello(name){
+    console.log(`Welcome to ${name}`);
+}
+sayHello("Anish Singh")
+function sum(a,b){
+
+    return a+b;
+}
+
+const findSum = (a,b) => a+b;
+console.log(`sum of 5 and 10 is ${findSum(5, 10)}`);
